a80a66bd7e2513fc38e9f92e2642ac62b472354d
ramesh Ramavaram <rameshramavaram@MacBook-Pro-3.local>
2025-01-28 23:04:49 -0800

    add tests

---
+++ src/pages/__tests__/SignIn.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import SignIn from '../SignIn';
+
+it('renders learn react link', () => {
+  render(<SignIn />);
+//  const linkElement = screen.getByText(/password/i);
+//  expect(linkElement).toBeInTheDocument();
+});
