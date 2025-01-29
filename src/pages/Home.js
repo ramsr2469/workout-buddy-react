41f0dd4b633e928d72795cbd85c68a2293ff0187
ramesh Ramavaram <rameshramavaram@MacBook-Pro-3.local>
2025-01-28 23:09:28 -0800

    fix errors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,7 +24,7 @@ function Home() {
     if (user) {
       fetchWorkOuts();
     }
-  }, [dispatch]);
+  }, [dispatch, user]);
   return (
     <div className="home">
       <div className="workouts">
